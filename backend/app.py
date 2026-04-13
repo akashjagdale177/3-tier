280f6542dd5d13fe271474d2e36db10d8dd87aa2
akashjagdale177 <akashjagdale177@gmail.com>
2026-04-13 23:36:21 +0530

    first deploy

--- backend/app.py
+++ backend/app.py
@@ -1,57 +1,16 @@
-from flask import Flask, jsonify, request
-from flask_cors import CORS
-import mysql.connector
-import time
-
+from flask import Flask, jsonify
 app = Flask(__name__)
-CORS(app)
-
-
-def get_connection():
-    for i in range(10):
-        try:
-            conn = mysql.connector.connect(
-                host="mysql-db",
-                user="root",
-                password="root123",
-                database="studentdb"
-            )
-            return conn
-        except:
-            time.sleep(2)
-    return None
-
 
 @app.route('/')
 def home():
-    return "Backend Running"
-
+    return "Backend Running Successfully on Render"
 
 @app.route('/students')
 def students():
-    conn = get_connection()
-    cursor = conn.cursor(dictionary=True)
-    cursor.execute("SELECT * FROM students")
-    data = cursor.fetchall()
-    conn.close()
-    return jsonify(data)
+    return jsonify([
+        {"id":1,"name":"Akash","course":"DevOps"},
+        {"id":2,"name":"Rahul","course":"Python"}
+    ])
 
-
-@app.route('/addstudent', methods=['POST'])
-def addstudent():
-    data = request.json
-
-    conn = get_connection()
-    cursor = conn.cursor()
-
-    sql = "INSERT INTO students (name, course, email) VALUES (%s, %s, %s)"
-    values = (data['name'], data['course'], data['email'])
-
-    cursor.execute(sql, values)
-    conn.commit()
-    conn.close()
-
-    return jsonify({"message": "Student Added Successfully"})
-
-
-app.run(host="0.0.0.0", port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
